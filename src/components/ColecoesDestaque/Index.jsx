6c3e7a9dd5c2169ea8cfce44d30e13b704837900
FRANCISCO WALLACE BARBOSA FERREIRA <FRANCISCO0657@DIGITALCOLLEGE.LOCAL>
2024-07-17 16:01:17 -0300

    Ajuste conflito

--- src/components/ColecoesDestaque/Index.jsx
+++ src/components/ColecoesDestaque/Index.jsx
@@ -1,5 +1,21 @@
 export default function ColecoesDestaque(){
     return(
-        <p>.</p>
+        <div class="container">
+            <div className="">
+                <h1>Camisetas</h1>
+            </div>
+            <div className="">
+                <h2>Calças</h2>
+            </div>
+            <div className="">
+                <h3>Bonés</h3>
+            </div>
+            <div className="">
+                <h4>Headphones</h4>
+            </div>
+            <div className="">
+                <h5>Tênis</h5>
+            </div>
+        </div>
     ) 
 }
