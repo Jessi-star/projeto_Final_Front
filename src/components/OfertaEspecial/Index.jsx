b76b2085acdeace0d8932539354c54f47ac892cb
Jessi-star <128174036+Jessi-star@users.noreply.github.com>
2024-07-17 15:40:48 -0300

    Oferta-Finalizada

--- src/components/OfertaEspecial/Index.jsx
+++ src/components/OfertaEspecial/Index.jsx
@@ -1,5 +1,31 @@
-export default function OfertaEspecial(){
-    return(
-        <p>.</p>
-    ) 
+export default function OfertaEspecial() {
+    return (
+        <div className="flex flex-row h-screen items-center justify-center">
+            <div className="w-1/2 h-full">
+                <img className="flex absolute left-20 bottom-[120px]" src="src/assets/img/nike.png" width={500} />
+            </div>
+            <div className="w-1/2 h-full">
+                
+                <p class="font-bold text-sm text-pink-600 my-1">Oferta Especial</p>
+                <div className="max-w-md">
+                <p class="font-bold text-4xl break-all">Air Jordan edição de
+                <br/> 
+                colecionador</p>
+                </div>
+                <div className="max-w-md">
+                    <p class="break-words text-sm my-3 break-all">Lorem ipsum dolor sit amet, consectetus adipiscing elit, 
+                    sed do 
+                    <br/>
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim
+                    <br/> 
+                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    <br/> 
+                    aliquip</p>
+                </div>
+                <button class="bg-pink-600 hover:bg-yellow-300 text-white font-bold py-1 px-11 rounded my-1"> Ver Oferta</button>
+            </div>
+        </div>
+
+    )
 }
