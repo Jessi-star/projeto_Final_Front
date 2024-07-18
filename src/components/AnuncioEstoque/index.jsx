df20f18bc32c4ca34ce74912fab9ca1354bdd1e2
icarorocha007 <icarorocha.igcr@gmail.com>
2024-07-18 14:51:32 -0300

    muda o tamanho da imagem

--- src/components/AnuncioEstoque/index.jsx
+++ src/components/AnuncioEstoque/index.jsx
@@ -1,25 +1,29 @@
 export default function AnuncioEstoque() {
     return (
         <div className="w-full h-screen flex flex-col">
-            <div className="w-full flex p-4">
-                <div className="w-1/2">
-                    <h2 className="text-5x1 my-1 text-orange-400">Melhores ofertas personalizadas</h2>
-                    <p className="font-bold text-4xl my-2">Queima de </p>
-                    <p className="font-bold text-4xl my-2">estoque Nike🔥 </p>
-                    <h1>Consequat culpa exercitaion mollit nisi excepteur do tempor</h1>
-                    <h1>aboris eiusmod irure consecteur.</h1>
-                    <button class="offer-button my-3">Ver Ofertas</button>
+            <div className="w-full flex px-[50px] pt-4">
+                <div className="w-1/2 flex flex-col items-center justify-center">
+                    <div className="flex flex-col ">
+                        <h2 className="text-5x1 my-1 text-orange-400">Melhores ofertas personalizadas</h2>
+                        <div className="break-all">
+                            <p className="font-bold text-6xl my-3 ">Queima de <br></br> estoque Nike🔥</p>
+                        </div>
+                        <h1>Consequat culpa exercitaion mollit nisi excepteur do tempor</h1>
+                        <h1>aboris eiusmod irure consecteur.</h1>
+                        <button class="offer-button my-3 bg-[#C92071] hover:bg-[#973e69] text-white cursor-pointer rounded-sm pl-2 w-[200px]"  >Ver Ofertas</button>
+                    </div>
+
                 </div>
-                <div className="ladoDireito w-1/2">
-                    <img src="src\image\jordan.png" width={300} />
+                <div className="ladoDireito w-1/2 flex items-start justify-start" >
+                    <img src="src/assets/img/airjordan.png" className="mr-10" />
                 </div>
             </div>
 
-            <div className="indicadores w-full flex items-center justify-center">
-                <div className="ponto ativo"></div>
-                <div className="ponto"></div>
-                <div className="ponto"></div>
-                <div className="ponto"></div>
+            <div className="indicadores w-full flex items-center justify-center gap-2">
+                <div className=" h-3 w-3 rounded-full ponto ativo bg-[#C92071]"></div>
+                <div className="h-3 w-3 rounded-full ponto bg-gray-500 cursor-pointer"></div>
+                <div className="h-3 w-3 rounded-full ponto bg-gray-500 cursor-pointer"></div>
+                <div className="h-3 w-3 rounded-full ponto bg-gray-500 cursor-pointer"></div>
             </div>
         </div>
 
