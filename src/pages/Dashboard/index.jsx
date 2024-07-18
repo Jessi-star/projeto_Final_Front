f48b56e061991bed0035d8d9009cab568fedf7db
Antonio-Luan <antonio.luan.m.novais@gmail.com>
2024-07-18 16:10:53 -0300

    add api

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,14 +3,16 @@ import Header from "../../components/Header";
 import AnuncioEstoque from "../../components/AnuncioEstoque";
 import ColecoesDestaque from "../../components/ColecoesDestaque/Index";
 import OfertaEspecial from "../../components/OfertaEspecial/Index";
+import ProdutosAlta from "../../components/ProdutosAlta/Index";
 
 export default function Dashboard (){
     return (
-        <div className="w-full h-screen flex flex-col">
+        <div className="w-full h-screen flex flex-col gap-5">
             <Header />
             <AnuncioEstoque /> 
-            <OfertaEspecial/> 
             <ColecoesDestaque/>
+            <ProdutosAlta/>
+            <OfertaEspecial/> 
 
             <Footer />
         </div>
