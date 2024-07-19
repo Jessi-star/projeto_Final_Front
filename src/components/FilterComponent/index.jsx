e740e36b9327c12412e83f7bef987a79c441d633
Jessi-star <128174036+Jessi-star@users.noreply.github.com>
2024-07-19 16:13:30 -0300

    Filter-Finalizado

--- src/components/FilterComponent/index.jsx
+++ src/components/FilterComponent/index.jsx
@@ -1,7 +1,12 @@
-export default function Filter() {
+export default function FilterComponent({text, titulo}) {
     return (
-        <div >
-                <h1>oi</h1>
+        <div className="flex  accent-pink-700 gap-5">
+            <p className="font-bold pt-8">{titulo}</p>
+            <div>
+            <input className="size-5" type="checkbox"  />
+            <label className="text-base ml-2">{text}</label>
+            </div>
+                
         </div>
     )
 }
